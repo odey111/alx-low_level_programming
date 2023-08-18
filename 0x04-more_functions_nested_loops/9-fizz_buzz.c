91e2598fb5a7d68166b3c74cf555df8ac076a15b
odey innocent <odeyinnocentodey@gmail.com>
2023-08-18 08:53:47 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,14 +5,15 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-    int i;
+        int i;
 
     for (i = 1; i <= 100; i++)
     {
-        if (i % 3 == 0 && i % 5 == 0)
-            printf("FizzBuzz");
+            if (i % 15 == 0)
+             printf("FizzBuzz");
         else if (i % 3 == 0)
             printf("Fizz");
         else if (i % 5 == 0)
