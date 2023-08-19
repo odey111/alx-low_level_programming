d437993eb4c6b23b00cf98287403750bd7555860
odey innocent <odeyinnocentodey@gmail.com>
2023-08-19 01:06:09 -0700

    Fizz-Buzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,43 +1,36 @@
 #include <stdio.h>
-#include "main.h"
-
 /**
- * main - Entry pint
- * Description: Prints numbers and words from 1 to 100,
- * Fizz for multiples of 3 and Buzz for multiples of 5
- * FizzBuzz for both 
- * Return: Always 0 (Success)
+ *main - replace multiples of 3 and 5.
+ *
+ *Return: 0.
  */
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 1; i <= 100; i++)
-    {
-        if (i % 3 == 0 && i % 5 == 0)
-        {
-            printf("FizzBuzz");
-        }
-        else if (i % 3 == 0)
-        {
-            printf("Fizz");
-        }
-        else if (i % 5 == 0)
-        {
-            printf("Buzz");
-        }
-        else
-        {
-            printf("%d", i);
-        }
-
-        if (i < 100)
-        {
-            printf(" ");
-        }
-    }
-
-    printf("\n");
-
-    return (0);
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 &&  i % 5 == 0)
+		{
+			printf("%s", "FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("%s", "Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("%s", "Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (i != 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
 }
